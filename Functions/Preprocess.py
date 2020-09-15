2acb603d440e1761dc7aff0c1bf966e8f618d699
Noam Wunch <noamwunch1@gmail.com>
2020-09-15 16:32:42 +0300

    many stuff

--- Functions/Preprocess.py
+++ Functions/Preprocess.py
@@ -20,7 +20,7 @@ def events_to_df(events_paths, label, max_ev=int(1e5), n_constits=15, PT_cut=(14
         sort = 0
     elif sort == "D0":
         sort = 11
-        
+
     if type(events_paths) != list:
         events_paths = [events_paths]
     for events_path in events_paths:
@@ -87,6 +87,87 @@ def events_to_df(events_paths, label, max_ev=int(1e5), n_constits=15, PT_cut=(14
                    "track_D0", "track_DZ", "track_errD0", "track_errDZ", "track_Xd", "track_Yd"]
 
     jets_df[track_feats] = jets_df[track_feats].applymap(lambda x: np.append(x[:n_constits], [0] * (n_constits - len(x))))
+
+    jets_df["label"] = label
+
+    return jets_df
+
+
+def tracks_to_df(events_paths, label, max_ev=int(1e5), n_constits=15, PT_cut=(140, 160), sort="PT"):
+    """Takes event list path (string) and returns a pandas Dataframe with jet info"""
+    # PT cut
+    PT_min = PT_cut[0]
+    PT_max = PT_cut[1]
+    ev_num = 0  # Event number
+    met = 0  # Missing transverse energy
+    jets_list = []  # List of jet features
+    jet1_info = []
+    jet2_info = []
+    jet1_accept = False
+    jet2_accept = False
+    jet1_track = []
+    jet2_track = []
+
+    if sort == "PT":
+        sort = 0
+    elif sort == "D0":
+        sort = 5
+
+    if type(events_paths) != list:
+        events_paths = [events_paths]
+    for events_path in events_paths:
+        with open(events_path) as events:
+            for line in events:
+                row = line.split()
+                # New event
+                if row[0] == "--":
+                    if ev_num > 0:
+                        if jet1_accept and jet1_track:
+                            jet1_track = np.array(jet1_track, dtype="float")
+                            jet1_track = jet1_track[jet1_track[:, sort].argsort()[::-1]]
+                            jets_list.append(jet1_info + list(jet1_track.T))
+                        if jet2_accept and jet2_track:
+                            jet2_track = np.array(jet2_track, dtype="float")
+                            jet2_track = jet2_track[jet2_track[:, sort].argsort()[::-1]]
+                            jets_list.append(jet2_info + list(jet2_track.T))
+                        jet1_accept = False
+                        jet2_accept = False
+                        jet1_track = []
+                        jet2_track = []
+                    ev_num += 1
+                    if ev_num > max_ev:
+                        break
+                    continue
+                # General jet info
+                if row[0] == "Jet":
+                    if int(row[1]) == 1:
+                        jet1_accept = (float(row[3]) > PT_min) and (float(row[3]) < PT_max)
+                        if jet1_accept:
+                            jet1_info = [ev_num, row[1], row[3], row[5], row[7]]
+                        continue
+                    if int(row[1]) == 2:
+                        jet2_accept = (float(row[3]) > PT_min) and (float(row[3]) < PT_max)
+                        if jet2_accept:
+                            jet2_info = [ev_num, row[1], row[3], row[5], row[7]]
+                        continue
+                # Constituents info
+                if row[0].isdigit():
+                    if jet1_accept and int(row[1]) == 1:
+                        jet1_track.append(row[2:] + [abs(float(row[5]))])
+                        continue
+                    if jet2_accept and int(row[1]) == 2:
+                        jet2_track.append(row[2:] + [abs(float(row[5]))])
+                        continue
+
+    jets_df = pd.DataFrame(jets_list,
+                           columns=["Event", "Jet", "jet_PT", "jet_Eta", "jet_Phi",
+                                    "track_PT", "track_Eta", "track_Phi", "track_D0", "track_DZ", "abs_D0"])
+    dtypes = {"Event": np.int, "Jet": np.int, "jet_PT": np.float, "jet_Eta": np.float, "jet_Phi": np.float}
+    jets_df = jets_df.astype(dtypes)
+    track_feats = ["track_PT", "track_Eta", "track_Phi", "track_D0", "track_DZ"]
+
+    jets_df[track_feats] = jets_df[track_feats].applymap(
+        lambda x: np.append(x[:n_constits], [0] * (n_constits - len(x))))
 
     jets_df["label"] = label
 
